bc8227ff3934c65680985db91d219e740ba6dae4
Shinnamon-Roll <chimonkung0@gmail.com>
2024-10-12 21:56:16 +0700

    fix : directory and feat : splice and slice

---
+++ KongRuksiam/ep11.jsx
@@ -0,0 +1,7 @@
+// const data = [10, 20, 30, 40, 50]
+// data.splice(1,3,"Yes-tood")     //remove first and last and replace remove
+// console.log(data)
+
+const data = [10, 20, 30, 40, 50]
+const lastData = data.slice(1, 3)    //first and last but before  [20, 30]
+console.log(lastData)
