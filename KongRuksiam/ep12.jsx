ea194e99b919f339d6ddb36771c098317f6b6961
Shinnamon-Roll <chimonkung0@gmail.com>
2024-10-12 22:06:24 +0700

    feat : for foreach forof

---
+++ KongRuksiam/ep12.jsx
@@ -0,0 +1,18 @@
+const data = [10, 20, 30, 40, 50]
+
+//normal for
+// for (let index = 0; index < data.length; index++) {
+//     console.log(data[index])
+// }
+
+
+//foreach
+// data.forEach(e => {     //foreact can't break and continue
+//     console.log(e)
+// })
+
+
+//for of
+for (const element of data) {
+    console.log(element)
+}
